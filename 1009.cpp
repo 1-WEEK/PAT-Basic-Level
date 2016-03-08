4fa56707f2375b4b2e5b8b70ca92421385db7117
Bayeszou <bayeszou@gmail.com>
2016-03-08 21:19:29 +0800

    fixed

--- 1009.cpp
+++ 1009.cpp
@@ -1,15 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
     string word[41] = {""};
-    int i = 0;
-    while( i < 41) {
+    int i = 0, j;
+    do {
         cin >> word[i];
         ++i;
-        if( cin.get() == '\n' ) break;
-    }
-    for ( int j = i-1; j > 0; --j) cout << word[j] << " ";
+    } while( i < 41 && cin.get() != '\n' );
+    for ( j = i-1; j > 0; --j ) cout << word[j] << " ";
     cout << word[0];
     return 0;
 }
